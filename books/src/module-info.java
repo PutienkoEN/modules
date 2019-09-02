7261a8c9ee4117aa188d28dbcbf5d36c46dd634f
Evgenii_Putienko <evgenii_putienko@epam.com>
2019-09-02 20:30:03 +0300

    Add requires and run script

--- books/src/module-info.java
+++ books/src/module-info.java
@@ -1,3 +1,3 @@
 module books {
-
+    requires jackson.databind;
 }
--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ CP+="lib/jackson-core-2.8.8.jar;"
 
 javac -cp $CP --module-path mods -d out --module-source-path "./*/src" -m books
 
-#java -cp "$CP;out" books.demo.Main
+java -cp "$CP" --module-path "mods;out" -m books/books.demo.Main
